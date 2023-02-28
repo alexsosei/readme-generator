173d0efd6af1ae89cf7971235a24e0f236e5ecd0
[alexander osei] <[valid.alexsosei@icloud.com]>
2023-02-28 12:33:02 +0000

    function-initialized

--- index.js
+++ index.js
@@ -2,20 +2,87 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const api = require("./utils/api");
 
 // array of questions for user
-const questions = [
+const questions = [{
+    type: "input",
+    message: "What is the title of the project",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the project about? Kindly provide detailed description of the project ",
+    name: "Description"
+}, {
+    type: "input",
+    message: "Table of content",
+    name: "Table of Content"
+}, {
+    type: "input",
+    message: "If require, provide details of the installations requirere",
+    name: "installation"
+}, {
+    type: "input",
+    message: "Provide instructions and examples of your project in use",
+    name: "usage"
+}, {
+    type: "list",
+    message: "Select the license for the project",
+    Selection: ['Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    name: "license"
+}, {
+    type: "input",
+    message: "Explain how other developers can contribute to the project",
+    name: "contributing"
+}, {
+    type: "input",
+    message: "Provide tests conducted for project",
+    name: "tests"
+}, {
+    type: "input",
+    message: "Provide contact details in case users have questions",
+    name: "questions"
+}
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success! Your README.md file has been reliably generated");
+    });
 }
+
+const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
 function init() {
-
+    try {
+        const data = inquirer.prompt(questions);
+        console.log("Your responses:", data);
+        console.log("Thank you for your response! Wait for program to initialise...");
+        
+}
+catch (error) {
+    console.log(error);
 }
 
 // function call to initialize program
+const userInfo = api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(data, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        writeFileAsync('ExampleREADME.md', markdown);
+
+};
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,30 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(data,userInfo) {
   return `# ${data.title}
-
+  https://github.com/${data.Username}/${data.Title}
+  #Description
+  ${data.Description}
+  #Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  # Installation
+  The following necessary dependencies must be installed to run the application.
+  # Usage 
+  In order to use this app, ${data.Usage}
+  #License
+  This project is licensed under the ${data.License} license.
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  #Contributing
+  Contributors: ${data.Contributing}
+  #Tests
+  The following is needed to run the test: ${data.tests}
+  #Questions
+  If you have any question about this repo, open an issue or contact ${data.Username}
+  
 `;
 }
 
